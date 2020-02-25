b68b1516d6d686cd6a62b46e660f119b221edac4
eduardo <e630603002@hotmail.com>
2020-02-25 19:01:00 +0000

    musica nueva

--- index.js
+++ index.js
@@ -94,7 +94,7 @@ let game = {
   highScore: 0,
   speed: 150,  
   yourScore: document.getElementById("yourScore"),
-  music: new Audio('./sound/ambiente.mp3'),
+  music: new Audio('./sound/ambiente2.ogg'),
   init: function () {
     let TABLE = document.getElementById("pixelCanvas");
     TABLE.innerHTML = '';
